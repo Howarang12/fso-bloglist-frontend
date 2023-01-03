1a8095f10f31b662316cef1888a8a20f1b3d2c68
Howard Wang <howard122696@gmail.com>
2023-01-02 23:25:43 -0800

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,6 +15,7 @@ const App = () => {
 
   return (
     <div>
+      <Login />
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Login = ({handleLogin, username, setUsername, password, setPassword}) => {
+const Login = () => {
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleLogin = (e) => {
+		e.preventDefault()
+		console.log(`Logging in ${username}`)
+	}
 	return (
 		<form onSubmit={handleLogin}>
+			<h1>log in to application</h1>
 			<div>
 				username
 				<input 
 					type="text" 
-					value
+					value={username}
+					onChange={e => setUsername(e.target.value)}
 				/>
 			</div>
 			<div>
 				password
 				<input 
 					type="text" 
-					value
+					value={password}
+					onChange={e => setPassword(e.target.value)}
 				/>
 			</div>
+			<button type="submit">Login</button>
 		</form>
 	)
 }
